1265727386a4eaba3b6aee37c210c6967f447e6d
Piyush Singh Thakur <161829619+piyushht2003@users.noreply.github.com>
2026-08-30 15:53:46 +0530

    Time: 18 ms (13.64%) | Memory: 69.7 MB (79.92%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
@@ -1,37 +1,40 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
-class Solution {
-    public boolean exists(TreeNode root, TreeNode target) {
-        if (root == null) return false;
-        if (root == target) return true;
-        return exists(root.left, target) || exists(root.right, target);
-    }
 
+class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         if (root == null) return null;
         if (root == p || root == q) return root;
 
-        boolean pLiesInLeft = exists(root.left, p);
-        boolean qLiesInLeft = exists(root.left, q);
+        TreeNode l = lowestCommonAncestor(root.left, p, q);
+        TreeNode r = lowestCommonAncestor(root.right, p, q);
 
-        // Dono left me hain -> left jao
-        if (pLiesInLeft && qLiesInLeft) {
-            return lowestCommonAncestor(root.left, p, q);
-        }
-        // Dono right me hain -> right jao
-        else if (!pLiesInLeft && !qLiesInLeft) {
-            return lowestCommonAncestor(root.right, p, q);
-        }
-        // Ek left me aur ek right me -> root hi LCA hai
-        else {
-            return root;
-        }
+        if (l != null && r != null) return root;
+        return (l == null) ? r : l;
     }
+    
+    // public boolean exists(TreeNode root, TreeNode target) {
+    //     if (root == null) return false;
+    //     if (root == target) return true;
+    //     return exists(root.left, target) || exists(root.right, target);
+    // }
+
+    // public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+    //     if (root == null) return null;
+    //     if (root == p || root == q) return root;
+
+    //     boolean pLiesInLeft = exists(root.left, p);
+    //     boolean qLiesInLeft = exists(root.left, q);
+
+    //     // Dono left me hain -> left jao
+    //     if (pLiesInLeft && qLiesInLeft) {
+    //         return lowestCommonAncestor(root.left, p, q);
+    //     }
+    //     // Dono right me hain -> right jao
+    //     else if (!pLiesInLeft && !qLiesInLeft) {
+    //         return lowestCommonAncestor(root.right, p, q);
+    //     }
+    //     // Ek left me aur ek right me -> root hi LCA hai
+    //     else {
+    //         return root;
+    //     }
+    // }
 }
