ea705fcab989b940465f765e97547d5216e11fd6
Piyush Singh Thakur <161829619+piyushht2003@users.noreply.github.com>
2026-09-11 17:21:10 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (32.47%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
@@ -15,14 +15,39 @@
  */
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
-       ArrayList<Integer> ans = new ArrayList<>();
-       dfs(root, ans);
-       return ans; 
-    }
-    public void dfs(TreeNode root, ArrayList<Integer> ans) {
-       if(root==null) return;
-       dfs(root.left, ans);
-       ans.add(root.val);
-       dfs(root.right, ans);
+        ArrayList<Integer> ans = new ArrayList<>();
+        Stack<TreeNode> st = new Stack<>();
+        TreeNode curr = root;
+        while(st.size()>0 || curr!=null){
+            if(curr!=null){
+                if(curr.left!=null){
+                    st.push(curr);
+                    curr = curr.left;
+                }
+                else{
+                    ans.add(curr.val);
+                    curr = curr.right;
+                }
+            }
+            else{
+                TreeNode top = st.pop();
+                ans.add(top.val);
+                curr = top.right;
+            }
+        }
+        return ans;
     }
 }
+// class Solution {
+//     public List<Integer> inorderTraversal(TreeNode root) {
+//        ArrayList<Integer> ans = new ArrayList<>();
+//        dfs(root, ans);
+//        return ans; 
+//     }
+//     public void dfs(TreeNode root, ArrayList<Integer> ans) {
+//        if(root==null) return;
+//        dfs(root.left, ans);
+//        ans.add(root.val);
+//        dfs(root.right, ans);
+//     }
+// }
