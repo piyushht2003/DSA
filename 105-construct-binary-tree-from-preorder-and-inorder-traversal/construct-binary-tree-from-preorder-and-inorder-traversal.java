84f2ea74ade867a2413d3a22a3147fc3a6874327
Piyush Singh Thakur <161829619+piyushht2003@users.noreply.github.com>
2026-09-12 14:40:23 +0530

    Time: 0 ms (100.00%) | Memory: 45.6 MB (78.63%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -1,37 +1,42 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+// class Solution {
+//     public TreeNode buildTree(int[] preorder, int[] inorder) {
+//         int n = inorder.length;
+//         return build(0,n-1,0,n-1,inorder,preorder);
+//     }
+//     public static TreeNode build(int preLo, int preHi, int inLo, int inHi, int inorder[], int preorder[]) {
+//         if(preLo > preHi || inLo > inHi) return null;
+//         int val = preorder[preLo];
+//         TreeNode root = new TreeNode(val);
+//         int r = 0;
+//         for(int i=inLo;i<=inHi;i++){ // locating root in inorder array
+//             if(inorder[i]==val){
+//                 r = i;
+//                 break;
+//             }
+//         }
+//         int cnt = r - inLo; // Number of elements in LST
+//         root.left = build(preLo+1,preLo+cnt,inLo,r-1,inorder,preorder);
+//         root.right = build(preLo+cnt+1,preHi,r+1,inHi,inorder,preorder);
+//         return root;
+//     }
+// }
 class Solution {
+    int pre = 0;
+    int in = 0;
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        int n = inorder.length;
-        return build(0,n-1,0,n-1,inorder,preorder);
+        return BST(preorder, inorder, Integer.MAX_VALUE);
     }
-    public static TreeNode build(int preLo, int preHi, int inLo, int inHi, int inorder[], int preorder[]) {
-        if(preLo > preHi || inLo > inHi) return null;
-        int val = preorder[preLo];
-        TreeNode root = new TreeNode(val);
-        int r = 0;
-        for(int i=inLo;i<=inHi;i++){ // locating root in inorder array
-            if(inorder[i]==val){
-                r = i;
-                break;
-            }
+
+    public TreeNode BST(int[] preorder, int[] inorder, int limit){
+        if(pre >= preorder.length) return null;
+        if(inorder[in] == limit){
+            in++;
+            return null;
         }
-        int cnt = r - inLo; // Number of elements in LST
-        root.left = build(preLo+1,preLo+cnt,inLo,r-1,inorder,preorder);
-        root.right = build(preLo+cnt+1,preHi,r+1,inHi,inorder,preorder);
+
+        TreeNode root = new TreeNode(preorder[pre++]);
+        root.left = BST(preorder, inorder, root.val);
+        root.right = BST(preorder, inorder, limit);
         return root;
     }
 }
