6f82e157f94fae85f792d3d291394577cb87ec4d
Piyush Singh Thakur <161829619+piyushht2003@users.noreply.github.com>
2026-09-20 15:09:49 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (6.11%) - LeetSync

---
+++ 1114-binary-search-tree-to-greater-sum-tree/binary-search-tree-to-greater-sum-tree.java
@@ -0,0 +1,19 @@
+class Solution {
+    public static void inorder(TreeNode root, ArrayList<TreeNode> arr ) {
+        if(root==null) return;
+        inorder(root.left, arr);
+        arr.add(root);
+        inorder(root.right, arr);
+    }
+    public TreeNode bstToGst(TreeNode root) {
+        ArrayList<TreeNode> arr = new ArrayList<>();
+        inorder(root, arr);
+        Collections.reverse(arr);
+        int sum = 0;
+        for(int i = 0; i<arr.size(); i++){
+            sum += arr.get(i).val;
+            arr.get(i).val = sum;
+        }
+        return root;
+    }
+}
