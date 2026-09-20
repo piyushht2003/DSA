d7d2ad58441b1fa99ea1b2fc3f684f146a8e08a5
Piyush Singh Thakur <161829619+piyushht2003@users.noreply.github.com>
2026-09-20 15:21:32 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (75.01%) - LeetSync

--- 1114-binary-search-tree-to-greater-sum-tree/binary-search-tree-to-greater-sum-tree.java
+++ 1114-binary-search-tree-to-greater-sum-tree/binary-search-tree-to-greater-sum-tree.java
@@ -1,19 +1,34 @@
 class Solution {
-    public static void inorder(TreeNode root, ArrayList<TreeNode> arr ) {
+    static int sum;
+    public void revInorder(TreeNode root) {
         if(root==null) return;
-        inorder(root.left, arr);
-        arr.add(root);
-        inorder(root.right, arr);
+        revInorder(root.right);
+        sum += root.val;
+        root.val = sum;
+        revInorder(root.left);
     }
     public TreeNode bstToGst(TreeNode root) {
-        ArrayList<TreeNode> arr = new ArrayList<>();
-        inorder(root, arr);
-        Collections.reverse(arr);
-        int sum = 0;
-        for(int i = 0; i<arr.size(); i++){
-            sum += arr.get(i).val;
-            arr.get(i).val = sum;
-        }
+        sum = 0;
+        revInorder(root);
         return root;
     }
 }
+// class Solution {
+//     public static void inorder(TreeNode root, ArrayList<TreeNode> arr ) {
+//         if(root==null) return;
+//         inorder(root.left, arr);
+//         arr.add(root);
+//         inorder(root.right, arr);
+//     }
+//     public TreeNode bstToGst(TreeNode root) {
+//         ArrayList<TreeNode> arr = new ArrayList<>();
+//         inorder(root, arr);
+//         Collections.reverse(arr);
+//         int sum = 0;
+//         for(int i = 0; i<arr.size(); i++){
+//             sum += arr.get(i).val;
+//             arr.get(i).val = sum;
+//         }
+//         return root;
+//     }
+// }
