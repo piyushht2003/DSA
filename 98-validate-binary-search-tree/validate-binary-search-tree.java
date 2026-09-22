e9aec4c593bcf53aac491de5b2797aebe1bf91eb
Piyush Singh Thakur <161829619+piyushht2003@users.noreply.github.com>
2026-09-22 14:47:46 +0530

    Time: 0 ms (100.00%) | Memory: 45.3 MB (35.09%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -1,47 +1,44 @@
-class Pair {
-    long max;
-    long min;
-
-    Pair(long max, long min) {
-        this.max = max;
-        this.min = min;
-    }
-}
-
 class Solution {
-    boolean flag;
-
     public boolean isValidBST(TreeNode root) {
-        flag = true;
-        maxMin(root);
-        return flag;
+        return validate(root, Long.MIN_VALUE, Long.MAX_VALUE);
     }
-
-    Pair maxMin(TreeNode root) {
-        if (root == null) return new Pair(Long.MIN_VALUE, Long.MAX_VALUE);
-
-        Pair left = maxMin(root.left);
-        Pair right = maxMin(root.right);
-
-        long max = Math.max(root.val, Math.max(left.max, right.max));
-        long min = Math.min(root.val, Math.min(left.min, right.min));
-
-        if (left.max >= root.val || right.min <= root.val) flag = false;
-
-        return new Pair(max, min);
+    private boolean validate(TreeNode node, long min, long max) {
+        if(node == null) return true;
+        if(node.val <= min || node.val >= max) return false;
+        return validate(node.left, min, node.val) && validate(node.right, node.val, max);
     }
 }
+
+// class Pair {
+//     long max;
+//     long min;
+
+//     Pair(long max, long min) {
+//         this.max = max;
+//         this.min = min;
+//     }
+// }
+
 // class Solution {
-//     long prev = Long.MIN_VALUE;
+//     boolean flag;
 
 //     public boolean isValidBST(TreeNode root) {
-//         if (root == null) return true;
-//         // left
-//         if (!isValidBST(root.left)) return false;
-//         // current
-//         if (root.val <= prev)return false;
-//         prev = root.val;
-//         // right
-//         return isValidBST(root.right);
+//         flag = true;
+//         maxMin(root);
+//         return flag;
+//     }
+
+//     Pair maxMin(TreeNode root) {
+//         if (root == null) return new Pair(Long.MIN_VALUE, Long.MAX_VALUE);
+
+//         Pair left = maxMin(root.left);
+//         Pair right = maxMin(root.right);
+
+//         long max = Math.max(root.val, Math.max(left.max, right.max));
+//         long min = Math.min(root.val, Math.min(left.min, right.min));
+
+//         if (left.max >= root.val || right.min <= root.val) flag = false;
+
+//         return new Pair(max, min);
 //     }
 // }
